5bc19e24d6132a1ce78f66ade40cec5ea2f3c2f9
aristotle <leventidis.a@husky.neu.edu>
2019-04-11 18:21:20 -0400

    Added responsiveness and scaling for the line charts as well

--- map/timeline.js
+++ map/timeline.js
@@ -285,74 +285,93 @@ function create_timeline(filename, x_attr, y_attr, line_charts_arr, line_chart_a
 
 // Returns x, xAxis and line
 function create_line_chart(divId, filename, x_attr, y_attr) {
-    var svg = d3.select(divId),
-    margin = {top: 20, right: 20, bottom: 40, left: 60},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
 
-    var x = d3.scaleLinear().range([0, width]),
-        y = d3.scaleLinear().range([height, 0]);
+    function redraw() {
+        line_div = $(divId).parent()[0];
+        $(divId).attr("width", line_div.clientWidth);
+        $(divId).attr("height", line_div.clientHeight); 
+        var svg = d3.select(divId),
+        margin = {top: 20, right: 20, bottom: 40, left: 60},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
 
-    var xAxis = d3.axisBottom(x),
-        yAxis = d3.axisLeft(y);
+        $(divId).empty();
 
-    var line = d3.line()
-        .x(function (d) { return x(d[x_attr]); })
-        .y(function (d) { return y(d[y_attr]); });
+        var x = d3.scaleLinear().range([0, width]),
+            y = d3.scaleLinear().range([height, 0]);
 
-    var clip = svg.append("defs").append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("x", 0)
-        .attr("y", 0); 
+        var xAxis = d3.axisBottom(x),
+            yAxis = d3.axisLeft(y);
 
-    var Line_chart = svg.append("g")
-        .attr("class", "focus")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-        .attr("clip-path", "url(#clip)");
+        var line = d3.line()
+            .x(function (d) { return x(d[x_attr]); })
+            .y(function (d) { return y(d[y_attr]); });
 
-    var focus = svg.append("g")
-        .attr("class", "focus")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var clip = svg.append("defs").append("svg:clipPath")
+            .attr("id", "clip")
+            .append("svg:rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 0)
+            .attr("y", 0); 
 
-    d3.csv(filename, type, function (error, data) {
-        if (error) throw error;
-      
-        x.domain(d3.extent(data, function(d) { return d[x_attr]; }));
-        y.domain([d3.min(data, function (d) { return d[y_attr]; }), d3.max(data, function (d) { return d[y_attr]; })]);
-      
-        focus.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+        var Line_chart = svg.append("g")
+            .attr("class", "focus")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .attr("clip-path", "url(#clip)");
 
+        var focus = svg.append("g")
+            .attr("class", "focus")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        focus.append("g")
-            .attr("class", "axis axis--y")
-            .call(yAxis)
+        d3.csv(filename, type, function (error, data) {
+            if (error) throw error;
+        
+            x.domain(d3.extent(data, function(d) { return d[x_attr]; }));
+            y.domain([d3.min(data, function (d) { return d[y_attr]; }), d3.max(data, function (d) { return d[y_attr]; })]);
+        
+            focus.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
 
-    
-        Line_chart.append("path")
-            .datum(data)
-            .attr("class", "line")
-            .attr("d", line);
+            focus.append("g")
+                .attr("class", "axis axis--y")
+                .call(yAxis)
+        
+            Line_chart.append("path")
+                .datum(data)
+                .attr("class", "line")
+                .attr("d", line);
 
-        svg.append("text")
-            .attr("x", width - 25)
-            .attr("y", height + margin.bottom + 10)
-            .style("font-size", "12px")
-            .text(x_attr);
+            svg.append("text")
+                .attr("x", width - 25)
+                .attr("y", height + margin.bottom + 10)
+                .style("font-size", "12px")
+                .text(x_attr);
 
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", margin.left/3)
-            .attr("dx", -2*margin.left)
-            .style("text-anchor", "middle")
-            .style("font-size", "12px")
-            .text(y_attr);
-    });
+            svg.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", margin.left/3)
+                .attr("dx", -2*margin.left)
+                .style("text-anchor", "middle")
+                .style("font-size", "12px")
+                .text(y_attr);
+        });
+
+        arr = [line, xAxis, x];
+        if (divId == "#line0") {
+            set_line_graph_globals(arr, 0);
+        }
+        else if (divId == "#line1") {
+            set_line_graph_globals(arr, 1);
+        }
+        else if (divId == "#line2") {
+            set_line_graph_globals(arr, 2);
+        }
+
+        return [line, xAxis, x];
+    }
 
     function type(d) {
         d[x_attr] = +d[x_attr];
@@ -360,29 +379,42 @@ function create_line_chart(divId, filename, x_attr, y_attr) {
         return d;
     }
 
-    return [line, xAxis, x];
+    // Draw for the first time to initialize.
+    var output = redraw();
+
+    // Redraw based on the new size whenever the browser window is resized.
+    window.addEventListener("resize", redraw);
+    return output;
 }
 
+function set_line_graph_globals(arr, idx) {
+    line_array[idx] = arr[0];
+    xAxis_array[idx] = arr[1];
+    x_array[idx] = arr[2];
+}
 
 // Create a timeline visualization
 // create_timeline_legacy("milestone3.csv","time","noise");
-var line_array = [],
-xAxis_array = [],
-x_array = [],arr;
+var line_array = [0,0,0];
+var xAxis_array = [0,0,0];
+var x_array = [0,0,0];
 arr = create_line_chart("#line0","milestone3.csv","time","noise");
-line_array.push(arr[0]);
-xAxis_array.push(arr[1]);
-x_array.push(arr[2]);
+// set_line_graph_globals(arr, 0);
+// line_array.push(arr[0]);
+// xAxis_array.push(arr[1]);
+// x_array.push(arr[2]);
 arr = create_line_chart("#line1","milestone3.csv","time","jamming_indicator");
-line_array.push(arr[0]);
-xAxis_array.push(arr[1]);
-x_array.push(arr[2]);
+// set_line_graph_globals(arr, 1);
+// line_array.push(arr[0]);
+// xAxis_array.push(arr[1]);
+// x_array.push(arr[2]);
 arr = create_line_chart("#line2","milestone3.csv","time","remaining");
-line_array.push(arr[0]);
-xAxis_array.push(arr[1]);
-x_array.push(arr[2]);
+// set_line_graph_globals(arr, 2);
+// line_array.push(arr[0]);
+// xAxis_array.push(arr[1]);
+// x_array.push(arr[2]);
 
-
+// console.log(line_array, xAxis_array, x_array);
 create_timeline("milestone3.csv","time","alt",line_array, xAxis_array, x_array);
 
 // var timeline_div = document.getElementById("timeline_div");
