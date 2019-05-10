e619a290315cb5424f621d6ce4249f6a6bcdeb41
David Saffo <dsaffo@luc.edu>
2019-05-10 11:34:13 -0400

    playing around with line thickness

--- js/map.js
+++ js/map.js
@@ -1,5 +1,10 @@
 //the map
-var map = L.map("map-canvas",{center:[0,0],zoom:19,maxZoom:25,minZoom:1});
+var map = L.map("map-canvas", {
+  center: [0, 0],
+  zoom: 19,
+  maxZoom: 25,
+  minZoom: 1
+});
 
 
 //some data holders
@@ -10,82 +15,96 @@ var window2 = [];
 var radioValue = 1;
 var yAttr = 'noise_per_ms';
 
- var div = d3.select(".tooltip")				
-        .style("opacity", 0);
+var div = d3.select(".tooltip")
+  .style("opacity", 0);
 
 
 
-    //
-  function draw(data){
+//
+function draw(data) {
 
-      startlat = data[0].lat / 10000000;
-      startlon = data[0].lon / 10000000;
-      map.remove();
-      map = L.map("map-canvas",{center:[startlat,startlon],zoom:19,maxZoom:25,minZoom:1});
-      var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        maxZoom: 20,
-        maxNativeZoom: 18
-      });
-      Esri_WorldTopoMap.addTo(map);
+  startlat = data[0].lat / 10000000;
+  startlon = data[0].lon / 10000000;
+  map.remove();
+  map = L.map("map-canvas", {
+    center: [startlat, startlon],
+    zoom: 19,
+    maxZoom: 25,
+    minZoom: 1
+  });
+  var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 20,
+    maxNativeZoom: 18
+  });
+  Esri_WorldTopoMap.addTo(map);
 
 
-      droneData = data.map(function(d){
-        endlat = d.lat/10000000
-        endlon =  d.lon/10000000
-        d.startlatLng = [+startlat,+startlon];
-        d.endlatLng = [+endlat,+endlon];
-        startlat = endlat
-        startlon = endlon
-        return d;
-      });
+  droneData = data.map(function (d) {
+    endlat = d.lat / 10000000;
+    endlon = d.lon / 10000000;
+    d.startlatLng = [+startlat, +startlon];
+    d.endlatLng = [+endlat, +endlon];
+    startlat = endlat;
+    startlon = endlon;
+    return d;
+  });
   //console.log(droneData);
   window2 = [0, droneData.length];
   //console.log(window2);
-  function selectColorScale(data, value, cmap){
+  function selectColorScale(data, value, cmap) {
 
-    max = d3.max(data, function(d){ return parseFloat(d[value]); });
-    min = d3.min(data, function(d){ return parseFloat(d[value]); });
-    
-    if (value == 'cpu_load' || value == 'ram_usage' || value == 'remaining'){
+    max = d3.max(data, function (d) {
+      return parseFloat(d[value]);
+    });
+    min = d3.min(data, function (d) {
+      return parseFloat(d[value]);
+    });
+
+    if (value === 'cpu_load' || value === 'ram_usage' || value === 'remaining') {
       max = 1;
       min = 0;
     }
-    
+
     //console.log(min,max)
     var colorScale = d3.scaleSequential(d3["interpolate" + cmap])
-        .domain([min, max]);
+      .domain([min, max]);
 
     return colorScale;
   }
 
-  function widthScale(data, value){
-    max = d3.max(data, function(d){ return parseFloat(d[value]); });
-    min = d3.min(data, function(d){ return parseFloat(d[value]); });
-    
-     
-    if (value == 'cpu_load' || value == 'ram_usage' || value == 'remaining'){
+  function widthScale(data, value) {
+    max = d3.max(data, function (d) {
+      return parseFloat(d[value]);
+    });
+    min = d3.min(data, function (d) {
+      return parseFloat(d[value]);
+    });
+
+
+    if (value === 'cpu_load' || value === 'ram_usage' || value === 'remaining') {
       max = 1;
       min = 0;
     }
-    
-    return widthScaler = d3.scaleLinear().domain([min, max]).range([1,40]);
+
+    return widthScaler = d3.scaleLinear().domain([min, max]).range([1, 40]);
   }
 
-  function drawMap(window){
+  function drawMap(window) {
 
-    var cmap = 'Reds'
+    var cmap = 'Reds';
 
-    if (radioValue == 1){
-      cmap = 'Reds'
-    }
-    else if (radioValue == 2){
-      cmap = 'Blues'
-    }
-    else{
-      cmap = 'Greens'
+    if (radioValue == 1) {
+      cmap = 'Reds';
+    } else if (radioValue == 2) {
+      cmap = 'Blues';
+    } else {
+      cmap = 'Greens';
     }
 
-    var pathOverlay = L.d3SvgOverlay(function(sel,proj){
+    var pathOverlay = L.d3SvgOverlay(function (sel, proj) {
+      
+      
+      var strokeWidth = map.getZoom()/3;
 
       d3.selectAll('.drone').remove();
       d3.selectAll('.fullPath').remove();
@@ -101,25 +120,33 @@ var yAttr = 'noise_per_ms';
 
       //our line generator
       var line = d3.line()
-        .x(function(d){ return proj.latLngToLayerPoint(d).x; })
-        .y(function(d){ return proj.latLngToLayerPoint(d).y; })
+        .x(function (d) {
+          return proj.latLngToLayerPoint(d).x;
+        })
+        .y(function (d) {
+          return proj.latLngToLayerPoint(d).y;
+        });
 
-       var smoothLine = d3.line()
-        .x(function(d){ return proj.latLngToLayerPoint(d.startlatLng).x; })
-        .y(function(d){ return proj.latLngToLayerPoint(d.startlatLng).y; })
+      var smoothLine = d3.line()
+        .x(function (d) {
+          return proj.latLngToLayerPoint(d.startlatLng).x;
+        })
+        .y(function (d) {
+          return proj.latLngToLayerPoint(d.startlatLng).y;
+        })
         .curve(d3.curveCatmullRom.alpha(0));
 
-      if (document.getElementById('ghostSwitch').checked == true){
+      if (document.getElementById('ghostSwitch').checked === true) {
         var fullPath = sel.selectAll('path')
-                          .data([droneData])
-                          .enter().append('path')
-                          .attr('class', 'fullPath')
-                          .attr('stroke', 'white')
-                          .attr('fill', 'none')
-                          .style('stroke-width', 5)
-                          .style('opacity', 0.5)
-                          .style("stroke-dasharray", "10,20")
-                          .attr('d', smoothLine);
+          .data([droneData])
+          .enter().append('path')
+          .attr('class', 'fullPath')
+          .attr('stroke', 'white')
+          .attr('fill', 'none')
+          .style('stroke-width', 3)
+          .style('opacity', 0.5)
+          .style("stroke-dasharray", "10,20")
+          .attr('d', smoothLine);
       }
 
       //timeWindow = window;
@@ -129,71 +156,88 @@ var yAttr = 'noise_per_ms';
       //  selectedData.push(droneData[i])
       //}
 
-      selectedData = droneData.slice(window[0],window[1] + 1);
+      selectedData = droneData.slice(window[0], window[1] + 1);
 
-      if (document.getElementById('colorSwitch').checked == true){
-        colorScale = selectColorScale(selectedData,yAttr,cmap)
+      if (document.getElementById('colorSwitch').checked === true) {
+        colorScale = selectColorScale(selectedData, yAttr, cmap);
+      } else {
+        colorScale = selectColorScale(droneData, yAttr, cmap);
       }
 
-      else{
-        colorScale = selectColorScale(droneData,yAttr,cmap)
-      }
-      
-     
+
 
       var path = sel.selectAll('path')
         .data(selectedData)
         .enter().append('path')
-        .attr('id', function(d){ return 'time:' + d.time; })
-        .attr('stroke', function(d) { return colorScale(parseFloat(d[yAttr])) })
+        .attr('id', function (d) {
+          return 'time:' + d.time;
+        })
+        .attr('stroke', function (d) {
+          return colorScale(parseFloat(d[yAttr]));
+        })
         .attr('class', 'drone')
-        .attr('d', function(d){ return line([d.startlatLng,d.endlatLng])})
-        .style('stroke-width', map.getZoom())
-        
-        .on('mouseover', function(d){
-            d3.select(this).style("cursor", "pointer");
-            d3.select(this).style('stroke-width', d3.select(this).style('stroke-width') * 2);
-            set_hover_line(d.time);
-            div.transition()		
-                .duration(200)		
-                .style("opacity", .9);		
-            div.html("Time: " + d.time + "s<br/> Value: "  + d[yAttr])	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
+        .attr('d', function (d) {
+          return line([d.startlatLng, d.endlatLng]);
         })
-        .on('mouseout', function(d){
-          div.transition()		
-                .duration(500)		
-                .style("opacity", 0);	
-          if (document.getElementById('widthSwitch').checked == true){
+        .style('stroke-width', strokeWidth)
 
-            d3.select(this).style('stroke-width', function(d){ return d[yAttr]});
-          }
-          else{
-            d3.select(this).style('stroke-width', map.getZoom());
-          }
-          set_hover_line_out();
+        .on('mouseover', function (d) {
+          d3.select(this).style("cursor", "pointer");
+          d3.select(this).style('stroke-width', d3.select(this).style('stroke-width') * 2);
+          set_hover_line(d.time);
+          div.transition()
+            .duration(200)
+            .style("opacity", 0.9);
+          div.html("Time: " + d.time + "s<br/> Value: " + d[yAttr])
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
         })
+        .on('mouseout', function (d) {
+          div.transition()
+            .duration(500)
+            .style("opacity", 0);
+          if (document.getElementById('widthSwitch').checked === true) {
 
-      if (document.getElementById('widthSwitch').checked == true){
-        //console.log('width switch')
-        if (document.getElementById('colorSwitch').checked == true){
+            if (document.getElementById('colorSwitch').checked === true) {
           var widthScaler = widthScale(selectedData, yAttr);
-        }
-        else{
+        } else {
           var widthScaler = widthScale(droneData, yAttr);
         }
-        path.style('stroke-width', function(d){return widthScaler(d[yAttr]);});
+        path.style('stroke-width', function (d) {
+          return widthScaler(d[yAttr]);
+        });
+          } else {
+            d3.select(this).style('stroke-width', strokeWidth);
+          }
+          set_hover_line_out();
+        });
+
+      if (document.getElementById('widthSwitch').checked === true) {
+        //console.log('width switch')
+        if (document.getElementById('colorSwitch').checked === true) {
+          var widthScaler = widthScale(selectedData, yAttr);
+        } else {
+          var widthScaler = widthScale(droneData, yAttr);
+        }
+        path.style('stroke-width', function (d) {
+          return widthScaler(d[yAttr]);
+        });
         //path.attr('stroke-width',  function(d){return d[yAttr];});
       }
 
 
 
       var circle = sel.append("circle")
-                      .attr("cx", function(d){return proj.latLngToLayerPoint(selectedData[selectedData.length - 1].endlatLng).x;})
-                      .attr("cy", function(d){return proj.latLngToLayerPoint(selectedData[selectedData.length - 1].endlatLng).y;})
-                      .attr("r", map.getZoom()/1.5)
-                      .attr('fill',function(d){return colorScale(selectedData[selectedData.length - 1][yAttr])});
+        .attr("cx", function (d) {
+          return proj.latLngToLayerPoint(selectedData[selectedData.length - 1].endlatLng).x;
+        })
+        .attr("cy", function (d) {
+          return proj.latLngToLayerPoint(selectedData[selectedData.length - 1].endlatLng).y;
+        })
+        .attr("r", map.getZoom() / 2)
+        .attr('fill', function (d) {
+          return colorScale(selectedData[selectedData.length - 1][yAttr]);
+        });
 
 
 
@@ -204,92 +248,91 @@ var yAttr = 'noise_per_ms';
 
 
 
-    }, options = {zoomDraw:false});
+    }, options = {
+      zoomDraw: false
+    });
 
-   pathOverlay.addTo(map);
+    pathOverlay.addTo(map);
   }
 
 
-//draw the path for the first time
-drawMap([0,droneData.length]);
+  //draw the path for the first time
+  drawMap([0, droneData.length]);
 
 
-map.on('zoomend', function() {
-  drawMap(window2);
-});
+  map.on('zoomend', function () {
+    drawMap(window2);
+  });
 
-window.addEventListener('timeline_update', function (e) {
-  //if(colorSwitch == true){
-  //  colorSwitch = false;
-  //  document.getElementById("colorSwitch").checked = false;
-  //}
-  drawMap(e.detail);
-});
+  window.addEventListener('timeline_update', function (e) {
+    //if(colorSwitch === true){
+    //  colorSwitch = false;
+    //  document.getElementById("colorSwitch").checked = false;
+    //}
+    drawMap(e.detail);
+  });
 
-document.getElementById("widthSwitch").addEventListener("change", function(){
-  drawMap(window2);
-});
-document.getElementById("colorSwitch").addEventListener("change", function(){
-  drawMap(window2);
-});
-document.getElementById("ghostSwitch").addEventListener("change", function(){
-  drawMap(window2);
-});
+  document.getElementById("widthSwitch").addEventListener("change", function () {
+    drawMap(window2);
+  });
+  document.getElementById("colorSwitch").addEventListener("change", function () {
+    drawMap(window2);
+  });
+  document.getElementById("ghostSwitch").addEventListener("change", function () {
+    drawMap(window2);
+  });
 
-// Initializing Materialize objects
-$(document).ready(function(){
+  // Initializing Materialize objects
+  $(document).ready(function () {
     $('.tabs').tabs();
-});
- $(document).ready(function(){
+  });
+  $(document).ready(function () {
     $('select').formSelect();
   });
 
-$(document).ready(function(){
-  $('.modal').modal();
-});
+  $(document).ready(function () {
+    $('.modal').modal();
+  });
 
-$(document).ready(function(){
-  $('.fixed-action-btn').floatingActionButton();
-});
+  $(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+  });
 
-$(".tab").click(function() {
-  id = this.id
-  console.log("tab clicked");
+  $(".tab").click(function () {
+    id = this.id;
+    console.log("tab clicked");
 
-  $("#radio1_input").prop("checked", true);
-  radioValue = 1;
-  yAttr = timeline_get_attribute(id, radioValue);
-  if (id == 'tab1') {
-    //console.log("Clicked", id);
-    tab_clicked(id);
-  }
-  else if (id == 'tab2') {
-    //console.log("Clicked", id);
-    tab_clicked(id);
-  }
-  else if (id == 'tab3') {
-    //console.log("Clicked", id);
-    tab_clicked(id);
-  }
-  else if (id == 'tab4') {
-    //console.log("Clicked", id);
-    tab_clicked(id);
-  }
-  drawMap(window2);
-})
+    $("#radio1_input").prop("checked", true);
+    radioValue = 1;
+    yAttr = timeline_get_attribute(id, radioValue);
+    if (id === 'tab1') {
+      //console.log("Clicked", id);
+      tab_clicked(id);
+    } else if (id === 'tab2') {
+      //console.log("Clicked", id);
+      tab_clicked(id);
+    } else if (id === 'tab3') {
+      //console.log("Clicked", id);
+      tab_clicked(id);
+    } else if (id === 'tab4') {
+      //console.log("Clicked", id);
+      tab_clicked(id);
+    }
+    drawMap(window2);
+  });
 
-// Need to listeners for radio change (one when user clicks and one when the tab is changed)!!
-$("input[name='group1']").change(function(){
-  var tab = $(".active").parent().attr('id');
-  radioValue = $(this).val();
-  yAttr = timeline_get_attribute(tab, radioValue);
-  //console.log(new_attr, radio_value);
-  drawMap(window2);
-});
+  // Need to listeners for radio change (one when user clicks and one when the tab is changed)!!
+  $("input[name='group1']").change(function () {
+    var tab = $(".active").parent().attr('id');
+    radioValue = $(this).val();
+    yAttr = timeline_get_attribute(tab, radioValue);
+    //console.log(new_attr, radio_value);
+    drawMap(window2);
+  });
 }
 
 
-function loadData(file){
+function loadData(file) {
   // $('.tabs a[href="#1"]').trigger('click');
   // var instance = M.Tabs.getInstance(document.getElementsByClassName('.tabs'));
   // instance.updateTabIndicator();
@@ -300,11 +343,13 @@ function loadData(file){
   var instance = M.Tabs.getInstance(el);
   instance.select('1');
 
-  
-  d3.csv(file, function(data){ draw(data);});
+
+  d3.csv(file, function (data) {
+    draw(data);
+  });
 }
 
-document.getElementById('logSelect').addEventListener('change', function(){
+document.getElementById('logSelect').addEventListener('change', function () {
   loadData(document.getElementById('logSelect').value);
   update_file_timeline(document.getElementById('logSelect').value);
   document.getElementById("colorSwitch").checked = false;
@@ -314,23 +359,23 @@ document.getElementById('logSelect').addEventListener('change', function(){
 
 
 
-function linkHoverOn(xAttr){
-  d3.select('[id="time:'+xAttr+'"]')
-    .style('stroke-width', d3.select('[id="time:'+xAttr+'"]')
-                            .style('stroke-width') * 2);
+function linkHoverOn(xAttr) {
+  d3.select('[id="time:' + xAttr + '"]')
+    .style('stroke-width', d3.select('[id="time:' + xAttr + '"]')
+      .style('stroke-width') * 2);
 }
 
-function linkHoverOut(xAttr){
+function linkHoverOut(xAttr) {
   // Make sure the xAttr value is valid
   if (xAttr >= 0) {
-    d3.select('[id="time:'+xAttr+'"]')
-    .style('stroke-width', d3.select('[id="time:'+xAttr+'"]')
-                            .style('stroke-width') / 2);
+    d3.select('[id="time:' + xAttr + '"]')
+      .style('stroke-width', d3.select('[id="time:' + xAttr + '"]')
+        .style('stroke-width') / 2);
   }
 }
 
-function setMetaDataInfo(file){
-  d3.csv('data/metadata.csv', function(data){
+function setMetaDataInfo(file) {
+  d3.csv('data/metadata.csv', function (data) {
     function filterCriteria(d) {
       fileName = d.dataset === file;
       return "data/" + fileName;
@@ -340,7 +385,7 @@ function setMetaDataInfo(file){
     var time = newData[0].LoggingDuration;
     var date = newData[0].Date;
     var dist = newData[0].Distance;
-    
+
     document.getElementById("metaData").innerHTML = "Airframe: " + airframe + " Date: " + date + " Duration: " + time + " Distance: " + dist;
   });
 }
